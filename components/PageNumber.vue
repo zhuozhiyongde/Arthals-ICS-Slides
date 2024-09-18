e93f62c703a371bc7a7456ca878a866fec91f101
Arthals <zhuozhiyongde@126.com>
2024-09-18 11:16:06 +0800

    🔧 chore: Delete components

--- components/PageNumber.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="absolute bottom-4 left-4 w-6 h-6 bg-sky-1 rounded-md text-center text-sky-4 dark:text-sky-5 dark:bg-sky-9">
-        <SlideCurrentNo class="w-full h-full" />
-    </div>
-</template>
