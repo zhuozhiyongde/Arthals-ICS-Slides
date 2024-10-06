b3f63603a98ece63c8bc52c688c4fc5bfbadb0e5
Arthals <zhuozhiyongde@126.com>
2024-10-07 05:20:53 +0800

    ✨ feat: Pm2

---
+++ ecosystem.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    apps: [
+        {
+            name: 'Slidev',
+            script: 'slidev --open',
+            log_date_format: 'YYYY-MM-DD HH:mm:ss',
+        },
+    ],
+};
