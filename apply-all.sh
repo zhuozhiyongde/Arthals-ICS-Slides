12495be6782e5fbb424fc1cb2b1ce5de8969e1a9
Arthals <zhuozhiyongde@126.com>
2025-05-13 20:01:30 +0800

    🐛 fix: Bugs

--- apply-all.sh
+++ apply-all.sh
@@ -1,16 +1,16 @@
 ./apply.sh 00
-# ./apply.sh 01
-# ./apply.sh 02
-# ./apply.sh 03
-# ./apply.sh 04
-# ./apply.sh 05
-# ./apply.sh 06
-# ./apply.sh 07
-# ./apply.sh 08
-# ./apply.sh 09
-# ./apply.sh 10
-# ./apply.sh 11
-# ./apply.sh 12
+./apply.sh 01
+./apply.sh 02
+./apply.sh 03
+./apply.sh 04
+./apply.sh 05
+./apply.sh 06
+./apply.sh 07
+./apply.sh 08
+./apply.sh 09
+./apply.sh 10
+./apply.sh 11
+./apply.sh 12
 
 ./update.sh
 
